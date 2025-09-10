47401d36156507c6fe66832792a3302698adbf57
athoy.nilima@ukaea.uk <athoy.nilima@ukaea.uk>
2025-09-10 09:04:58 +0100

    Move make_cell_array to neutronics_axissymetric

--- bluemira/radiation_transport/neutronics/neutronics_axisymmetric.py
+++ bluemira/radiation_transport/neutronics/neutronics_axisymmetric.py
@@ -18,8 +18,19 @@ import numpy as np
 
 from bluemira.base.look_and_feel import bluemira_print
 from bluemira.base.parameter_frame import Parameter, ParameterFrame, make_parameter_frame
-from bluemira.codes.openmc.make_csg import BluemiraNeutronicsCSG, make_cell_arrays
+from bluemira.codes.openmc.make_csg import (
+    BlanketCellArray,
+    BluemiraNeutronicsCSG,
+    CellStage,
+    DivertorCellArray,
+    make_coils,
+    make_radiation_shield_box,
+    make_universe_box,
+    make_void_cells,
+    round_up_next_openmc_ids,
+)
 from bluemira.codes.openmc.material import MaterialsLibrary
+from bluemira.geometry.constants import D_TOLERANCE
 from bluemira.geometry.plane import calculate_plane_dir
 from bluemira.geometry.tools import get_wire_plane_intersect, make_polygon
 from bluemira.radiation_transport.neutronics.make_pre_cell import PreCell
@@ -33,7 +44,6 @@ if TYPE_CHECKING:
     from numpy import typing as npt
 
     from bluemira.base.reactor import ComponentManager
-    from bluemira.codes.openmc.make_csg import CellStage
     from bluemira.geometry.wire import BluemiraWire
     from bluemira.radiation_transport.neutronics.geometry import TokamakDimensions
     from bluemira.radiation_transport.neutronics.make_pre_cell import (
@@ -72,6 +82,7 @@ class ReactorGeometry:
     ----------
     lower_divertor_inner_wire:
         The plasma-facing side of the divertor.
+        For single null configuration, this is the only divertor
     panel_break_points:
         The start and end points for each first-wall panel
         (for N panels, the shape is (N+1, 2)).
@@ -84,10 +95,12 @@ class ReactorGeometry:
     """
 
     lower_divertor_inner_wire: BluemiraWire
+    # For single null configuration, this is the only divertor
     panel_break_points: npt.NDArray
     vacuum_vessel_inner_wire: BluemiraWire
     vacuum_vessel_outer_wire: BluemiraWire
     customised_geometry: Any | None = None
+    # In case the user has a tokamak of different Geometry
 
 
 @dataclass
@@ -263,6 +276,20 @@ class NeutronicsReactor(ABC):
         blanket_discretisation: int = 10,
         divertor_discretisation: int = 5,
     ):
+        """
+        Initialises the Neutronics Reactor.
+
+        If geometry_type = GeometryType.SN_INTEGRATED, the arguments
+        first_wall, and panel_points will not be used as they are
+        considered within the blanket component.
+
+        For custom reactors, the user has the option to provide with
+        first_wall as a seperate component, with divertor considered within it
+        or not.
+
+        However, they have to make their own custom methods for cell-making or
+        getting wires, the methods are defined as an abstract method here.
+        """
         bluemira_print("Creating axis-symmetric neutronics model")
 
         self.params = make_parameter_frame(params, self.param_cls)
@@ -284,7 +311,12 @@ class NeutronicsReactor(ABC):
         self, blanket_discretisation, divertor_discretisation, snap_to_horizontal_angle
     ) -> CellStage:
         if self.geometry_type == GeometryType.CUSTOM:
-            # User's own method
+            # User's own methods
+            self._pre_cell_stage = self._cut_and_create_pre_cell_stage_custom(
+                blanket_discretisation=blanket_discretisation,
+                divertor_discretisation=divertor_discretisation,
+                snap_to_horizontal_angle=snap_to_horizontal_angle,
+            )
             return self._create_cell_stage_custom(
                 blanket_discretisation=blanket_discretisation,
                 divertor_discretisation=divertor_discretisation,
@@ -292,6 +324,27 @@ class NeutronicsReactor(ABC):
                 control_id=True,
             )
 
+        self._pre_cell_stage = self._cut_and_create_pre_cell_stage(
+            blanket_discretisation=blanket_discretisation,
+            divertor_discretisation=divertor_discretisation,
+            snap_to_horizontal_angle=snap_to_horizontal_angle,
+        )
+
+        return self._make_cell_arrays(
+            csg=BluemiraNeutronicsCSG(),
+            control_id=True,
+        )
+
+    def _cut_and_create_pre_cell_stage(
+        self, blanket_discretisation, divertor_discretisation, snap_to_horizontal_angle
+    ) -> PreCellStage:
+        """
+        Method to perform all intial cutting and pre-cell making
+
+        Returns
+        -------
+        PreCellStage
+        """
         cutting = CuttingStage(
             blanket=PanelsAndExteriorCurve(
                 self.geom.panel_break_points,
@@ -316,23 +369,127 @@ class NeutronicsReactor(ABC):
             snap_to_horizontal_angle=snap_to_horizontal_angle,
         )
 
-        pre_cell_stage = PreCellStage(
+        return PreCellStage(
             blanket=blanket.straighten_exterior(preserve_volume=True), divertor=divertor
         )
 
-        # setup importnat properties from precell stage
-        self.bounding_box = pre_cell_stage.bounding_box()
-        self.half_bounding_box = pre_cell_stage.half_bounding_box()
-        self.blanket_pre_cell_array = pre_cell_stage.blanket
-        self.divertor_pre_cell_array = pre_cell_stage.divertor
+    def _make_cell_arrays(
+        self,
+        csg: BluemiraNeutronicsCSG,
+        *,
+        control_id: bool = False,
+    ) -> CellStage:
+        """
+        Make pre-cell arrays for the blanket and the divertor.
 
-        return make_cell_arrays(
-            pre_cell_stage=pre_cell_stage,
-            csg=BluemiraNeutronicsCSG(),
-            materials=self.material_library,
-            tokamak_dimensions=self.tokamak_dimensions,
-            control_id=True,
+        Parameters
+        ----------
+        materials:
+            library containing information about the materials
+        tokamak_dimensions:
+            A parameter
+            :class:`bluemira.radiation_transport.neutronics.params.TokamakDimensions`,
+            Specifying the dimensions of various layers in the blanket, divertor, and
+            central solenoid.
+        control_id: bool
+            Whether to set the blanket Cells and surface IDs by force or not.
+            With this set to True, it will be easier to understand where each cell came
+            from. However, it will lead to warnings and errors if a cell/surface is
+            generated to use a cell/surface ID that has already been used respectively.
+            Keep this as False if you're running openmc simulations multiple times in one
+            session.
+
+        Returns
+        -------
+        CellStage
+        """
+        # determine universe_box
+
+        z_max, z_min, r_max, r_min = self._pre_cell_stage.half_bounding_box()
+
+        z_min_adj = z_min - D_TOLERANCE
+        z_max_adj = z_max + D_TOLERANCE
+        r_max_adj = r_max + D_TOLERANCE
+
+        rad_shield_wall_tk = self.tokamak_dimensions.rad_shield.wall
+
+        # make the universe box, incorporates the radiation shield wall
+        universe = make_universe_box(
+            csg,
+            z_min_adj - rad_shield_wall_tk,
+            z_max_adj + rad_shield_wall_tk,
+            r_max_adj + rad_shield_wall_tk,
+            control_id=control_id,
         )
+
+        blanket = BlanketCellArray.from_pre_cell_array(
+            self._pre_cell_stage.blanket,
+            self.material_library,
+            self.tokamak_dimensions,
+            csg,
+            control_id=control_id,
+        )
+
+        # change the cell and surface id register before making the divertor.
+        # (ids will only count up from here.)
+        if control_id:
+            round_up_next_openmc_ids()
+
+        divertor = DivertorCellArray.from_pre_cell_array(
+            self._pre_cell_stage.divertor,
+            self.material_library,
+            self.tokamak_dimensions.divertor,
+            csg=csg,
+            override_start_end_surfaces=(blanket[0].ccw_surface, blanket[-1].cw_surface),
+            # ID cannot be controlled at this point.
+        )
+
+        # make the plasma cell and the exterior void.
+        if control_id:
+            round_up_next_openmc_ids()
+
+        cs, tf = make_coils(
+            csg,
+            r_min - self.tokamak_dimensions.cs_coil.thickness,
+            self.tokamak_dimensions.cs_coil.thickness,
+            z_min_adj,
+            z_max_adj,
+            self.material_library,
+        )
+        # make the radiation shield wall
+        # which is a hollow of the universe box
+        rad_shield = make_radiation_shield_box(
+            csg,
+            z_min_adj,
+            z_max_adj,
+            r_max_adj,
+            universe,
+            self.material_library,
+        )
+        plasma, ext_void = make_void_cells(
+            csg,
+            universe=universe,
+            blanket=blanket,
+            divertor=divertor,
+            central_solenoid=cs,
+            tf_coils=tf,
+            rad_shield=rad_shield,
+            control_id=control_id,
+        )
+
+        cell_stage = CellStage(
+            blanket=blanket,
+            divertor=divertor,
+            tf_coils=tf,
+            cs_coil=cs,
+            plasma=plasma,
+            radiation_shield=rad_shield,
+            ext_void=ext_void,
+            universe=universe,
+        )
+        cell_stage.set_volumes()
+
+        return cell_stage
 
     def plot_2d(self, *args, **kwargs) -> Axes:
         """
@@ -349,8 +506,8 @@ class NeutronicsReactor(ABC):
 
         show = kwargs.pop("show", True)
         ax = kwargs.pop("ax", None)
-        ax = self.blanket_pre_cell_array.plot_2d(*args, ax=ax, show=False, **kwargs)
-        return self.divertor_pre_cell_array.plot_2d(*args, ax=ax, show=show, **kwargs)
+        ax = self._pre_cell_stage.blanket.plot_2d(*args, ax=ax, show=False, **kwargs)
+        return self._pre_cell_stage.divertor.plot_2d(*args, ax=ax, show=show, **kwargs)
 
     @abstractmethod
     def _get_wires_from_components(
@@ -365,6 +522,13 @@ class NeutronicsReactor(ABC):
         ...
 
     @abstractmethod
+    def _cut_and_create_pre_cell_stage_custom(
+        self, blanket_discretisation, divertor_discretisation, snap_to_horizontal_angle
+    ) -> PreCellStage:
+        """Customised Pre-cell making stage"""
+        ...
+
+    @abstractmethod
     def _create_cell_stage_custom(
         self,
         blanket_discretisation: int,
@@ -373,7 +537,19 @@ class NeutronicsReactor(ABC):
         *,
         control_id: bool = False,
     ) -> CellStage:
-        """Customised Pre-cell making stage"""
+        """Customised Cell making stage"""
+        ...
+
+    @abstractmethod
+    def _make_cell_arrays_custom(
+        self,
+        csg: BluemiraNeutronicsCSG,
+        materials: MaterialsLibrary,
+        tokamak_dimensions: TokamakDimensions,
+        *,
+        control_id: bool = False,
+    ) -> CellStage:
+        """Customised cell making stage"""
         ...
 
     @abstractmethod
